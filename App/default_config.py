cced9b7abf575102cc0ba46d510d64e97b92e676
Jaheim <caesariejaheim@gmail.com>
2024-04-21 22:57:37 -0400

    Increased Expire Time

--- App/default_config.py
+++ App/default_config.py
@@ -1,4 +1,4 @@
 SQLALCHEMY_DATABASE_URI = "postgresql://info_2602_project_db_zgvv_user:mYnuGAQDMxWtRcq6xYLeH4MGcLrGlbZN@dpg-co5m9bu3e1ms73b8gd4g-a.oregon-postgres.render.com/info_2602_project_db_zgvv"
 SECRET_KEY = "secret key"
-JWT_ACCESS_TOKEN_EXPIRES = 7
+JWT_ACCESS_TOKEN_EXPIRES = 20
 ENV = "PRODUCTION"
